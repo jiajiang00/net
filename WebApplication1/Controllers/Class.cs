6d6c33bd98d534e9c326eaa37fd4343324b0d0b3
asd <aa>
2018-07-11 12:21:53 +0800

    1

---
+++ WebApplication1/Controllers/Class.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+namespace netDemo.Controllers
+{
+    [Route("[controller]")]
+    public class ClassController : Controller
+    {
+        [HttpGet("index")]
+        public string index()
+        {
+            return Environment.GetEnvironmentVariable("asd");
+        }
+    }
+
+}
+
